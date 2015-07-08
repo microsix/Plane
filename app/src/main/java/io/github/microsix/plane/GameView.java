d1c703a7c8e47c2a004a804dc24e41edca3618a1
lgrush <lgrush@qq.com>
2015-07-08 21:52:15 +0800

    Move the location of chess from line to cell.

--- app/src/main/java/io/github/microsix/plane/GameView.java
+++ app/src/main/java/io/github/microsix/plane/GameView.java
@@ -9,23 +9,29 @@ import android.graphics.Color;
 import android.graphics.EmbossMaskFilter;
 import android.graphics.MaskFilter;
 import android.graphics.Paint;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
 
 public class GameView extends View {
 
+    private final String TAG = "GameView";
     private int startX = 0;
     private int startY = 0;
-    private int GRID_WIDTH = GlobalData.getWidth()/12;
-    private int GRID_MOD = GlobalData.getWidth()%12;
-    private int GRID_NUM = 13;
+    private int GRID_WIDTH = GlobalData.getWidth()/11;
+    private int GRID_MOD = GlobalData.getWidth()%11;
+    private int GRID_NUM = 11;
     private Paint paint = null;
 
     private int[][] chess = new int[GRID_NUM][GRID_NUM];
     private int CHESS_BLACK = 1;//表示棋子的颜色，1代表黑色，2代表白色，0达标没有棋子
     private int CHESS_WHITE = 2;
+    private int CHESS_GRAY = 3;
+    private int CHESS_RED = 4;
     private int chess_flag = 0;//用于记录上一次下的棋子的颜色，1为黑色，2为白色，0是刚开始下棋,上一次没下棋子
+
+    private int whichPlane = 1;
 
     public GameView(Context context) {
         super(context);
@@ -34,15 +40,10 @@ public class GameView extends View {
         paint.setAntiAlias(true);//设置画笔去锯齿，没有此语句，画的线或图片周围不圆滑
 
         // 为了不让棋盘的边界与屏幕的边界完全重合，需要让棋盘的边界离屏幕边界一定距离。
-        if(GRID_MOD == 0){
-            startX = GRID_WIDTH/2;
-            startY = GRID_WIDTH/2;
-            GRID_NUM--;
-        }
-        else{
-            startX = GRID_MOD/2;
-            startY = GRID_MOD/2;
-        }
+        Log.d(TAG, "GlobalData.getWidth() = " + GlobalData.getWidth() + ", GlobalData.getWidth() = " + GlobalData.getWidth());
+
+        startX = (GRID_MOD+GRID_WIDTH)/2;
+        startY = (GRID_MOD+GRID_WIDTH)/2;
     }
 
     @Override
@@ -64,19 +65,29 @@ public class GameView extends View {
         paint.setMaskFilter(emboss);
 
         //绘制棋子
-        for(int i=0;i<GRID_NUM;i++)
+        for(int i=1;i<GRID_NUM;i++)
         {
-            for(int j=0;j<GRID_NUM;j++)
+            for(int j=1;j<GRID_NUM;j++)
             {
                 if(chess[i][j] == CHESS_BLACK)
                 {
                     paint.setColor(Color.BLACK);//黑色画笔，画黑棋
-                    canvas.drawCircle(startX+i*GRID_WIDTH,startY+j*GRID_WIDTH ,GRID_WIDTH/2-3, paint);
+                    canvas.drawCircle(startX+i*GRID_WIDTH-GRID_WIDTH/2,startY+j*GRID_WIDTH-GRID_WIDTH/2,GRID_WIDTH/2-3, paint);
                 }
                 if(chess[i][j] == CHESS_WHITE)
                 {
                     paint.setColor(Color.WHITE);//白色画笔，画白棋
-                    canvas.drawCircle(startX+i*GRID_WIDTH,startY+j*GRID_WIDTH ,GRID_WIDTH/2-3, paint);
+                    canvas.drawCircle(startX+i*GRID_WIDTH-GRID_WIDTH/2,startY+j*GRID_WIDTH-GRID_WIDTH/2,GRID_WIDTH/2-3, paint);
+                }
+                if(chess[i][j] == CHESS_GRAY)
+                {
+                    paint.setColor(Color.GRAY);//黑色画笔，画黑棋
+                    canvas.drawCircle(startX+i*GRID_WIDTH-GRID_WIDTH/2,startY+j*GRID_WIDTH-GRID_WIDTH/2,GRID_WIDTH/2-3, paint);
+                }
+                if(chess[i][j] == CHESS_RED)
+                {
+                    paint.setColor(Color.RED);//白色画笔，画白棋
+                    canvas.drawCircle(startX+i*GRID_WIDTH-GRID_WIDTH/2,startY+j*GRID_WIDTH-GRID_WIDTH/2,GRID_WIDTH/2-3, paint);
                 }
             }
         }
@@ -96,22 +107,23 @@ public class GameView extends View {
         else
         {
             //根据点击的位置，从而获知在棋盘上的哪个位置，即是数组的脚标
-            int index_x = Math.round((touchX-startX)/GRID_WIDTH);
-            int index_y = Math.round((touchY-startY)/GRID_WIDTH);
+            int index_x = (int)Math.ceil((touchX-startX)/GRID_WIDTH);
+            int index_y = (int)Math.ceil((touchY-startY)/GRID_WIDTH);
 
-            if(chess_flag == 0)
-            {//此句表示在最开始下棋的时候每次都是黑棋先下
-                chess[index_x][index_y] = CHESS_BLACK;
-                chess_flag = CHESS_BLACK;
+            switch (whichPlane) {
+                case 1:
+                    if (chess[index_x][index_y] == 0) {
+                        chess[index_x][index_y] = CHESS_WHITE;
+                    } else {
+                        chess[index_x][index_y] = 0;
+                    }
+                    break;
+                case 2:
 
-            }else if( chess_flag == CHESS_BLACK && chess[index_x][index_y] == 0)
-            {
-                chess[index_x][index_y] = CHESS_WHITE;
-                chess_flag = CHESS_WHITE;
-            }else if(chess_flag == CHESS_WHITE && chess[index_x][index_y] == 0)
-            {
-                chess[index_x][index_y] = CHESS_BLACK;
-                chess_flag = CHESS_BLACK;
+                    break;
+                case 3:
+
+                    break;
             }
         }
 
